85f624ed7d2c29270d93f9548860c210da619ab7
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-06-26 19:52:16 -0400

    part 1 complete

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+//replaced starter code with parameters//
+function newImage(url, left, bottom) {
+  let object = document.createElement("img");
+  object.src = url;
+  object.style.position = "fixed";
+  object.style.left = left + "px";
+  object.style.bottom = bottom + "px";
+  document.body.append(object);
+  return object;
+}
+
+function newItem(url, left, bottom) {
+  let object = newImage(url, left, bottom);
+
+  object.addEventListener("dblclick", function () {
+    object.remove();
+  });
+}
+//invoke newImage, 'passing' appropriate arguments
+newImage("assets/green-character.gif", 100, 100);
+newImage("assets/pine-tree.png", 450, 200);
+newImage("assets/tree.png", 200, 300);
+newImage("assets/pillar.png", 350, 100);
+newImage("assets/crate.png", 150, 200);
+newImage("assets/well.png", 500, 425);
+
+//[newItem function]adds items for player to pick up
+newItem("assets/sword.png", 500, 405);
+newItem("assets/shield.png", 165, 185);
+newItem("assets/staff.png", 600, 100);
+
+//starter code//
+//create greenCharacter//*
+// let greenCharacter = document.createElement("img");
+// greenCharacter.src = "assets/green-character.gif";
+// greenCharacter.style.position = "fixed";
+// greenCharacter.style.left = "100px";
+// greenCharacter.style.bottom = "100px";
+// document.body.append(greenCharacter);
+
+// let pineTree = document.createElement("img");
+// pineTree.src = "assets/pine-tree.png";
+// pineTree.style.position = "fixed";
+// pineTree.style.left = "450px";
+// pineTree.style.bottom = "200px";
+// document.body.append(pineTree);
+
+//sword added later
+// let sword = document.createElement("img");
+// sword.src = "assets/sword.png";
+// sword.position = "fixed";
+// sword.left = "500px";
+// sword.bottom = "405px";
+// document.body.append(sword);
+
+// sword.addEventListener("click", function () {
+//   sword.remove();
+// });
